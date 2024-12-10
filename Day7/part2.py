aea43e69593ba30893a5f561a3670a5042f6f13e
u05730dg <daniel.groves@student.manchester.ac.uk>
2024-12-10 18:03:30 +0000

    message same as prior

---
+++ Day7/part2.py
@@ -0,0 +1,50 @@
+from itertools import product
+
+with open("input.txt") as f:
+    lines = [line.strip() for line in f]
+
+lines = [line.split(": ") for line in lines]
+
+sums = []
+nums = []
+
+def check_sum(sum, nums, operations):
+    if len(nums) == 1:
+        if sum == nums[0]:
+            return True
+        else:
+            return False
+        
+    total = nums[0]
+
+    for i in range(len(nums[1:])):
+        if operations[i] == "+":
+            total += nums[i+1]
+        elif operations[i] == "*":
+            total *= nums[i+1]
+        elif operations[i] == "||":
+            total = int(str(total) + str(nums[i+1]))
+
+    if total == sum:
+        return True
+    else:
+        return False
+
+
+for line in lines:
+    sums.append(int(line[0]))
+    nums.append([int(i) for i in line[1].split(" ")])
+
+total = 0
+
+for i in range(len(nums)):
+    poss_operations = list(product(["+", "*", "||"], repeat=len(nums[i])-1))
+    poss_operations = [list(poss_operation) for poss_operation in poss_operations]
+
+    for poss_operation in poss_operations:
+        if check_sum(sums[i], nums[i], poss_operation):
+            total += sums[i]
+            break
+
+
+print(total)
