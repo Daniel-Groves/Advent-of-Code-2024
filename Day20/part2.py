3cceac6161014c1052c5117a0e96e77d92d74c2b
u05730dg <daniel.groves@student.manchester.ac.uk>
2024-12-20 15:19:41 +0000

    day 20 part 2 done, also takes a good few mins to run :/

--- Day20/part2.py
+++ Day20/part2.py
@@ -2,7 +2,7 @@ from collections import deque, defaultdict
 
 grid = [list(line) for line in open("input.txt").read().split("\n")]
 
-MAX_CHEAT = 1
+MAX_CHEAT = 20
 
 def length_to_end(grid, start, end):
     queue = deque()
@@ -24,18 +24,23 @@ def length_to_end(grid, start, end):
 
     return None, []
 
-def length_with_cheat(grid, start, end, cheat_wall, path):
-    saves = [0]
+def length_with_cheat(start, end, path):
+    saved = path.index(end) - path.index(start) - 2
+    if saved <= 0:
+        return 0
+    return saved
 
-    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        nx, ny = wall[0] + dx, wall[1] + dy
-        ox, oy = wall[0] - dx, wall[1] - dy
-        if (nx, ny) in path and (ox, oy) in path:
-            saves.append(abs(path.index((nx, ny)) - path.index((ox, oy))) - 2)
+def get_taxi_distance(start, end):
+    return abs(start[0] - end[0]) + abs(start[1] - end[1]) + 1
 
-    return max(saves)
-
-walls = []
+def get_coords_within_taxi_distance(x, y, distance):
+    coords = []
+    for dx in range(-distance, distance + 1):
+        dy_max = distance - abs(dx)
+        for dy in range(-dy_max, dy_max + 1):
+            if y + dy >= 0 and y + dy < len(grid) and x + dx >= 0 and x + dx < len(grid[0]) and grid[y + dy][x + dx] != "#":
+                coords.append((x + dx, y + dy))
+    return coords
 
 for y, row in enumerate(grid):
     for x, cell in enumerate(row):
@@ -43,26 +48,18 @@ for y, row in enumerate(grid):
             start = (x, y)
         elif cell == "E":
             end = (x, y)
-        elif cell == "#":
-            walls.append((x, y))
+
 
 steps, path = length_to_end(grid, start, end)
 
 saved_count = 0
-counts = defaultdict(int)
-print(path)
-print(len(walls))
 
-# for wall in walls:
-#     saved = length_with_cheat(grid, start, end, wall, path)
-#     print(saved)
-#     if saved:
-#         counts[saved] += 1
-#     if saved >= 100:
-#         saved_count += 1
-
-for path_point in path:
-
+for start in path:
+    for end in get_coords_within_taxi_distance(start[0], start[1], MAX_CHEAT):
+        if end not in path: continue
+        if get_taxi_distance(start, end) > MAX_CHEAT + 2: continue
+        saved = length_with_cheat(start, end, path) - get_taxi_distance(start, end) + 3
+        if saved >= 100:
+            saved_count += 1
 
 print(saved_count)
-print(counts)
